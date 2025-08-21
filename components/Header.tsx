9a82556ff49cef99adf21d81dd6666fd04a59c3d
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-08-21 21:38:04 +0100

    Add EnvelopeIcon to mobile menu dialog and enhance link styles for better interaction

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
+import { EnvelopeIcon } from "@heroicons/react/16/solid";
 import NavIcon from "./NavIcon/NavIcon";
 import "@/styles/globals.css";
 
@@ -35,7 +36,7 @@ export default function Header() {
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 z-999 cursor-pointer"
           >
             <span className="sr-only">Open main menu</span>
-            <NavIcon open={mobileMenuOpen} color={'foreground'} />
+            <NavIcon open={mobileMenuOpen} color={"foreground"} />
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
@@ -55,33 +56,36 @@ export default function Header() {
         onClose={setMobileMenuOpen}
         className="lg:hidden z-50"
       >
-
-          <DialogPanel className="fixed top-0 inset-y-0 right-0 z-50 w-full overflow-y-auto bg-primary p-6 sm:max-w-[280px]">
-            <div className="flex items-center justify-end">
-              <button
-                type="button"
-                onClick={() => toggleMobileMenu()}
-                className="w-10 rounded-md p-4 mr-2.5 z-999 cursor-pointer "
-              >
-                <span className="sr-only">Close menu</span>
-              </button>
-            </div>
-            <div className="mt-8 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10 dark:divide-white/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-background hover:bg-secondary/10 transition-colors duration-300"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
+        <DialogPanel className="fixed top-0 inset-y-0 right-0 z-50 w-full h-screen overflow-y-auto bg-primary p-6 sm:max-w-[280px]">
+          <div className="flex items-center justify-end">
+            <button
+              type="button"
+              onClick={() => toggleMobileMenu()}
+              className="w-10 rounded-md p-4 mr-2.5 z-999 cursor-pointer "
+            >
+              <span className="sr-only">Close menu</span>
+            </button>
+          </div>
+          <div className="flow-root">
+            <div className="-my-6 divide-y divide-gray-500">
+              <div className="space-y-2 py-6 mt-8">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-background hover:bg-background/5 hover:brightness-125 duration-300 transition-all  hover:scale-105"
+                  >
+                    {item.name}
+                  </a>
+                ))}
               </div>
             </div>
-          </DialogPanel>
+            <div className="absolute bottom-8 cursor-pointer">
+              <EnvelopeIcon className="h-6 w-6 text-background transition-transform duration-300 hover:scale-125" />
+              <span className="sr-only">Email</span>
+            </div>
+          </div>
+        </DialogPanel>
       </Dialog>
     </header>
   );
