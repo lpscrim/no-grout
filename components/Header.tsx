69be3f01358812375e191716f9329537cb5146aa
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-08-21 21:12:01 +0100

    Update Header and NavIcon styles for improved layout and consistency

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
     <header className="bg-primary text-background z-999">
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 z-999"
+        className="mx-auto flex max-w-7xl items-center justify-between py-4 px-4 lg:py-6 lg:px-8 z-999"
       >
         <a href="#" className="-m-1.5 p-1.5 text-xl font-bold ">
           No Grout About It
