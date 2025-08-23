4995167e6fece463f2c1591db9b18af86d1e7f71
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-08-23 10:48:54 +0100

    Refactor Home component for improved readability and structure; adjust spacing and formatting for better code clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,33 @@
 import { ArrowDownIcon } from "@heroicons/react/16/solid";
 export default function Home() {
   return (
-    <section id="hero" className="relative bg-primary bg-[url('/diagonal-striped-brick.png')] min-h-[100vh] flex flex-col justify-center items-center">
+    <section
+      id="hero"
+      className="relative bg-primary bg-[url('/diagonal-striped-brick.png')] min-h-[100vh] flex flex-col justify-center items-center"
+    >
       <div className="group text-center flex flex-row sm:flex-col justify-center items-center">
-        <p className="text-2xl sm:text-3xl sm:mb-5 text-background opacity-0 group-hover:opacity-50 duration-2500 hover:opacity-100">Bespoke</p>
+        <p className="text-2xl sm:text-3xl sm:mb-5 text-background opacity-0 group-hover:opacity-50 duration-2500 hover:opacity-100">
+          Bespoke
+        </p>
         <div className="pl-5 text-center text-6xl items-center font-bold flex flex-col sm:flex-row">
           <h1 className="text-accent">No&nbsp;</h1>
-        <h1 className="text-background"> Grout&nbsp;</h1>
-        <h1 className="text-accent"> About&nbsp;</h1>
-        <h1 className="text-background">It&nbsp;</h1>
+          <h1 className="text-background"> Grout&nbsp;</h1>
+          <h1 className="text-accent"> About&nbsp;</h1>
+          <h1 className="text-background">It&nbsp;</h1>
+        </div>
+        <p className="text-2xl sm:text-3xl text-background sm:mt-6 ml-2 sm:ml-0 opacity-0 group-hover:opacity-50 duration-2500 hover:opacity-100">
+          {" "}
+          Tiling
+        </p>
       </div>
-      <p className="text-2xl sm:text-3xl text-background sm:mt-6 ml-2 sm:ml-0 opacity-0 group-hover:opacity-50 duration-2500 hover:opacity-100"> Tiling</p>
-    </div>
-    <div className="absolute bottom-10 left-10">
-      <ArrowDownIcon className="h-6 w-6 text-background animate-bounce" />
-    </div>
-  </section>
-    
+      <div className="absolute bottom-10 max-w-7xl w-full">
+        <div className="absolute left-10 lg:left-0">
+          <ArrowDownIcon className="h-6 w-6 text-background animate-bounce" />
+        </div>
+        <div className="absolute invisible lg:visible right-10 xl:right-0">
+          <ArrowDownIcon className="h-6 w-6 text-background animate-bounce" />
+        </div>
+      </div>
+    </section>
   );
 }
