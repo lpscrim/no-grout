586bcc6d364cd3fbe2c25f7fdc8e01e3ed1088cd
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-10-24 12:39:41 +0100

    feat: add 'Reviews' link to navigation for improved site accessibility

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,6 +14,7 @@ gsap.registerPlugin(ScrollTrigger);
 const navigation = [
   { name: "Home", href: "/#home" },
   { name: "Projects", href: "/projects" },
+  { name: "Reviews", href: "/#testimonials" },
   { name: "Contact", href: "/#contact" },
 ];
 
