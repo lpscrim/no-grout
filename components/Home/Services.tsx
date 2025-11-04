d64552f19d8c0f30243dd28fe395fa9cbc7cc377
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-11-04 13:42:36 +0000

    fix: add missing 'Services' link to navigation for improved site accessibility

--- components/Home/Services.tsx
+++ components/Home/Services.tsx
@@ -34,6 +34,16 @@ const services = [
     description: "Comprehensive heating for floor areas.",
     image: "/hall.jpg",
   },
+  {
+    title: "Floor Heating",
+    description: "Comprehensive heating for floor areas.",
+    image: "/hall.jpg",
+  },
+  {
+    title: "Floor Heating",
+    description: "Comprehensive heating for floor areas.",
+    image: "/hall.jpg",
+  },
 ];
 
 export default function Services() {
@@ -42,29 +52,29 @@ export default function Services() {
 
   return (
     <section id="services" className=" bg-accent section-light">
-      <div className="px-6 sm:px-8 lg:px-16 max-w-screen-xl mx-auto py-18 md:py-24 lg:py-32">
-        <h2 className="text-4xl sm:text-5xl font-bold mb-18 lg:mb-20 mt-10 text-center text-secondary">
+      <div className="px-6 sm:px-8 lg:px-16 max-w-screen-xl mx-auto py-18 lg:py-24">
+        <h2 className="text-4xl sm:text-5xl font-bold mb-14 mt-10 text-center text-secondary">
           Our Services
         </h2>
          <p className="text-lg max-w-xl text-foreground font-light text-center mx-auto mb-16">
           Each project is custom tailored, based on each client needs. We are
           ready to take on any kind of projects, be that small or complex
         </p>
-        <div className="grid md:grid-cols-2 gap-20 items-center">
-          <div className="flex justify-center items-center h-[300px] md:h-[700px]">
+        <div className="grid lg:grid-cols-2 gap-20 items-center">
+          <div className="flex justify-center items-center h-[300px] lg:h-[550px]">
             <Image
               src={imageSrc}
               width={500}
               height={800}
               alt="Service Example"
-              className="w-full max-w-xl h-[350px] rounded-xs md:h-[700px] object-cover transition-all duration-300"
+              className="w-full max-w-xl h-[350px] rounded-xs lg:h-[550px] object-cover transition-all duration-300"
             />
           </div>
-          <ul className="grid grid-cols-2 md:grid-cols-1 gap-2 md:gap-4 xl:gap-5 max-w-xl mx-auto">
+          <ul className="grid grid-cols-2 lg:grid-cols-2 gap-2 lg:gap-4 xl:gap-2.5 max-w-xl mx-auto">
             {services.map((service, idx) => (
               <li
                 key={service.title}
-                className={`bg-background/50 p-4 md:p-5 rounded-xs cursor-pointer transition-all duration-200 ${hoveredIdx === idx ? " bg-background/70" : "bg-background/50"}`}
+                className={`bg-background/50 p-4 lg:p-5 rounded-xs cursor-pointer transition-all duration-200 ${hoveredIdx === idx ? " bg-background/70" : "bg-background/50"}`}
                 onMouseEnter={() => setHoveredIdx(idx)}
                 onClick={() => setHoveredIdx(idx)}
               >
