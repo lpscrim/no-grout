0261a663322d3757baffd1a4a24b2ac6a12d9eaa
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-11-24 18:03:06 +0000

    Refactor code structure for improved readability and maintainability

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -1,22 +1,34 @@
 "use client";
 import { ParallaxBanner } from "react-scroll-parallax";
 import AnimatedOnView from "@/components/Functions/AnimatedOnView";
+import { useEffect, useState } from "react";
 
 export default function About() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
   return (
     <section
       id="about"
-      className="relative min-h-[110lvh] flex justify-end items-start overflow-hidden section-light bg-accent"
+      className="relative min-h-[105lvh] flex justify-center items-center overflow-hidden section-light bg-accent"
     >
       <div className="absolute inset-0">
         <ParallaxBanner
-          layers={[{ image: "/hall.jpg", speed: -50 }]}
-          className="min-h-[200lvh] w-full"
+          layers={[{ image: "/hall2.svg", speed: isMobile ? 0 : -50 }]}
+          className="min-h-[150lvh] w-full blur-sm"
         />
       </div>
       <div>
         <AnimatedOnView animationClass="opacity-0 slide-in-right">
-          <div className="relative min-h-[115vh] z-10 bg-accent p-6 w-[50vw] sm:w-[40vw] lg:w-[35vw] xl:w-[25vw] flex flex-col justify-center text-left">
+          <div className="relative rounded-xs my-auto z-10 bg-accent p-18 w-[90vw] sm:w-[80vw] lg:w-[65vw] xl:w-[45vw] flex flex-col justify-center text-left">
             <h2 className="text-4xl sm:text-5xl font-bold mb-12 text-secondary">
               About Us
             </h2>
